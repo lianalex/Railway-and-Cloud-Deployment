45296a2cc1791e566d8b4d40eb2dc8f14171ba91
Alex Liang <alexliang@u.boisestate.edu>
2023-04-26 15:05:24 -0600

    getRandomString

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -10,6 +10,7 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 @SpringBootApplication
 @Controller
@@ -30,8 +31,9 @@ public class GettingStartedApplication {
     String database(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
+            statement.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
+
 
             final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
             final var output = new ArrayList<>();
@@ -46,6 +48,24 @@ public class GettingStartedApplication {
             model.put("message", t.getMessage());
             return "error";
         }
+    }
+
+    // Modify the getRandomString() method to mimic the functionality of the randomly generated string that should be output by the getRandomString() method -> Read from DB: 2023-04-26 20:38:06.780407 uuhwrigbta
+    // Specifically, every time the URL is accessed:
+    // the timestamp of the request and a randomly generated string should be inserted into a table and
+    // all existing records from the table should be retrieved and displayed in the web page
+
+    private String getRandomString() {
+        int length = 10;
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            char randomChar = (char) ('a' + random.nextInt(26));
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
     }
 
     public static void main(String[] args) {
